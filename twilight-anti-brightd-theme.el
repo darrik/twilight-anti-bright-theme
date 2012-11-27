65faea2ff5e7caf14c657c46592204e1147f1f62
Rikard Glans <rikard@ecx.se>
2012-11-27 12:41:55 +0100

    For the most part I can't stand random background colours.

--- twilight-anti-brightd-theme.el
+++ twilight-anti-brightd-theme.el
@@ -53,7 +53,7 @@
 ;;
 ;;; Code:
 
-(deftheme twilight-anti-bright
+(deftheme twilight-anti-brightd
   "A soothing light-on-dark theme.")
 
 (let ((background "#14191f")
@@ -84,52 +84,52 @@
       )
 
   (custom-theme-set-faces
-   'twilight-anti-bright
+   'twilight-anti-brightd
 
    ;; Basics
    `(default ((t (:background ,background :foreground ,foreground))))
    `(cursor ((t (:background ,cursor))))
    `(region ((t (:background ,selection))))
-   `(highlight ((t (:foreground ,blue-3 :background ,blue-3bg))))
+   `(highlight ((t (:foreground ,blue-3))))
    `(hl-line ((t (:background ,hl-line))))
-   `(minibuffer-prompt ((t (:foreground ,orange-1 :background ,orange-1bg))))
-   `(escape-glyph ((t (:foreground ,purple-1 :background , purple-1bg))))
+   `(minibuffer-prompt ((t (:foreground ,orange-1))))
+   `(escape-glyph ((t (:foreground ,purple-1))))
 
    ;; Font-lock stuff
-   `(font-lock-builtin-face ((t (:foreground ,yellow-1 :background ,yellow-1bg))))
-   `(font-lock-constant-face ((t (:foreground ,purple-1 :background ,purple-1bg))))
-   `(font-lock-comment-face ((t (:foreground ,comment :background ,gray-1bg :italic t))))
-   `(font-lock-doc-face ((t (:foreground ,gray-1 :background ,gray-1bg))))
-   `(font-lock-doc-string-face ((t (:foreground ,gray-1 :background ,gray-1bg))))
-   `(font-lock-function-name-face ((t (:foreground ,red-1 :background ,red-1bg))))
-   `(font-lock-keyword-face ((t (:foreground ,orange-1 :background ,orange-1bg))))
-   `(font-lock-negation-char-face ((t (:foreground ,yellow-1 :background ,yellow-1bg))))
-   `(font-lock-preprocessor-face ((t (:foreground ,orange-1 :background ,orange-1bg))))
-   `(font-lock-string-face ((t (:foreground ,green-1 :background ,green-1bg))))
-   `(font-lock-type-face ((t (:foreground ,red-2 :background ,red-2bg :bold nil))))
-   `(font-lock-variable-name-face ((t (:foreground ,blue-1 :background ,blue-1bg))))
-   `(font-lock-warning-face ((t (:foreground ,red-2 :background ,red-2bg))))
+   `(font-lock-builtin-face ((t (:foreground ,yellow-1))))
+   `(font-lock-constant-face ((t (:foreground ,purple-1))))
+   `(font-lock-comment-face ((t (:foreground ,comment))))
+   `(font-lock-doc-face ((t (:foreground ,gray-1))))
+   `(font-lock-doc-string-face ((t (:foreground ,gray-1))))
+   `(font-lock-function-name-face ((t (:foreground ,red-1))))
+   `(font-lock-keyword-face ((t (:foreground ,orange-1))))
+   `(font-lock-negation-char-face ((t (:foreground ,yellow-1))))
+   `(font-lock-preprocessor-face ((t (:foreground ,orange-1))))
+   `(font-lock-string-face ((t (:foreground ,green-1))))
+   `(font-lock-type-face ((t (:foreground ,red-2))))
+   `(font-lock-variable-name-face ((t (:foreground ,blue-1))))
+   `(font-lock-warning-face ((t (:foreground ,red-2))))
 
    ;; UI related
-   `(link ((t (:foreground ,blue-1 :background ,blue-1bg))))
+   `(link ((t (:foreground ,blue-1))))
    `(fringe ((t (:background ,gray-1bg))))
-   `(mode-line ((t (:foreground ,blue-1 :background ,blue-2bg))))
-   `(mode-line-inactive ((t (:foreground ,blue-4 :background ,gray-4))))
+   `(mode-line ((t (:foreground ,blue-1))))
+   `(mode-line-inactive ((t (:foreground ,blue-4))))
    `(vertical-border ((t (:background ,background :foreground ,gray-5))))
 
    ;; Linum
    `(linum ((t (:foreground ,gray-2 :background ,gray-1bg))))
 
    ;; show-paren-mode
-   `(show-paren-match ((t (:foreground ,orange-1 :background ,orange-1bg))))
-   `(show-paren-mismatch ((t (:foreground ,red-2bg :background ,red-2))))
+   `(show-paren-match ((t (:foreground ,orange-1))))
+   `(show-paren-mismatch ((t (:foreground ,red-2bg))))
 
    ;; ido
-   `(ido-only-match ((t (:foreground ,green-1 :background ,green-1bg))))
-   `(ido-subdir ((t (:foreground ,purple-1 :background ,purple-1bg))))
+   `(ido-only-match ((t (:foreground ,green-1))))
+   `(ido-subdir ((t (:foreground ,purple-1))))
 
    ;; whitespace-mode
-   `(whitespace-empty ((t (:foreground ,yellow-1bg :background ,yellow-1))))
+   `(whitespace-empty ((t (:foreground ,yellow-1bg))))
    `(whitespace-hspace ((t (:foreground ,gray-2))))
    `(whitespace-indentation ((t (:foreground ,gray-2))))
    `(whitespace-line ((t (:background ,gray-2))))
@@ -137,7 +137,7 @@
    `(whitespace-space ((t (:foreground ,gray-2))))
    `(whitespace-space-after-tab ((t (:foreground ,gray-2))))
    `(whitespace-tab ((t (:foreground ,gray-2))))
-   `(whitespace-trailing ((t (:foreground ,red-1bg :background ,red-1))))
+   `(whitespace-trailing ((t (:foreground ,red-1bg))))
 
    ;; flyspell-mode
    `(flyspell-incorrect ((t (:underline ,red-2))))
@@ -153,21 +153,21 @@
    `(highlight-indentation-current-column-face ((t (:background ,gray-4))))
 
    ;; ECB
-   `(ecb-default-general-face ((t (:foreground ,gray-3 :background ,gray-1bg))))
-   `(ecb-default-highlight-face ((t (:foreground ,red-1 :background ,red-1bg))))
-   `(ecb-method-face ((t (:foreground ,red-1 :background ,red-1bg))))
+   `(ecb-default-general-face ((t (:foreground ,gray-3))))
+   `(ecb-default-highlight-face ((t (:foreground ,red-1))))
+   `(ecb-method-face ((t (:foreground ,red-1))))
    `(ecb-tag-header-face ((t (:background ,blue-2bg))))
 
    ;; org-mode
-   `(org-date ((t (:foreground ,purple-1 :background ,purple-1bg))))
-   `(org-done ((t (:foreground ,green-1 :background ,green-1bg))))
-   `(org-hide ((t (:foreground ,gray-2 :background ,gray-1bg))))
-   `(org-link ((t (:foreground ,blue-1 :background ,blue-1bg))))
-   `(org-todo ((t (:foreground ,red-1 :background ,red-1bg))))
+   `(org-date ((t (:foreground ,purple-1))))
+   `(org-done ((t (:foreground ,green-1))))
+   `(org-hide ((t (:foreground ,gray-2))))
+   `(org-link ((t (:foreground ,blue-1))))
+   `(org-todo ((t (:foreground ,red-1))))
    )
 
   (custom-theme-set-variables
-   'twilight-anti-bright
+   'twilight-anti-brightd
 
    ;; ;; Fill Column Indicator mode
    `(fci-rule-color ,gray-6)
@@ -187,6 +187,6 @@
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
-(provide-theme 'twilight-anti-bright)
+(provide-theme 'twilight-anti-brightd)
 
 ;;; twilight-anti-bright-theme.el ends here
